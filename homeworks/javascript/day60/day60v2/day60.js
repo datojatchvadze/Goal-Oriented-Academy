cd22d73b201a1159906bd4ec1a089a34c7c3c9f2
datojatchvadze <138619309+datojatchvadze@users.noreply.github.com>
2024-02-17 12:50:39 +0400

    Add files via upload

---
+++ homeworks/javascript/day60/day60v2/day60.js
@@ -0,0 +1,16 @@
+let question1 = document.getElementById("question1");
+question1.addEventListener('click',function(){
+    question1.textContent = "What is Frontend Mentor, and how will it help me?Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.";
+})
+let question2 = document.getElementById("question2");
+question2.addEventListener('click',function(){
+    question2.textContent = "Is Frontend Mentor free?Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.";
+})
+let question3 = document.getElementById("question3");
+question3.addEventListener('click',function(){
+    question3.textContent = "Can I use Frontend Mentor projects in my portfolio? Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!";
+})
+let question4 = document.getElementById("question4");
+question4.addEventListener('click',function(){
+    question4.textContent = "How can I get help if I'm stuck on a Frontend Mentor challenge? The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.";
+})
